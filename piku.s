ef2bd545fcb1bbb1ab53fd3d32407af1c85804c2
Celestine Co <celestineco98@hotmail.com>
2018-04-06 15:23:50 -0700

    Completed README and piku lab

--- piku.s
+++ piku.s
@@ -5,20 +5,31 @@
 main:
     @ Stack the return address (lr) in addition to a dummy register (ip) to
     @ keep the stack 8-byte aligned.
-    push    {ip, lr}
+    push    {ip, lr} 
 
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
-    ldr     r0, =message
+    ldr     r0, =line1
+    bl      printf
+
+    ldr     r0, =line2
+    bl      printf
+
+    ldr     r0, =line3
     bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
     mov     r0, #0      @ Return 0.
+
     @ Pop the dummy ip to reverse our alignment fix, and pop the original lr
-    @ value directly into pc — the Program Counter — to return.
-    pop     {ip, pc}
+    @ value directly into pc ?~@~T the Program Counter ?~@~T to return.
+    pop     {ip, pc} 
 
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
-message:
-    .asciz  "Hello, world.\n"
+line1:
+    .asciz  "Oh, raspberry pi,\n"
+line2:
+    .asciz  "Acclaimed utilization\n"
+line3:
+    .asciz  "Though not edible\n"
